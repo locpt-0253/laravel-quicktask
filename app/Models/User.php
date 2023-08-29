2c9586a3f4c4fabfba9acfa0ffcc098193b74c28
MatsuoRotsuki <loclienhadonganh@gmail.com>
2023-08-29 15:36:38 +0700

    [Chapter 4]: Accessors, Mutators, Scopes

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Post;
+use App\Models\Scopes\ActiveScope;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -42,6 +45,31 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected function fullName(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->attributes['first_name'] . ' ' . $this->attributes['last_name'],
+        );
+    }
+
+    protected function username(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => $value,
+            set: fn (string $value) => Str::slug($value),
+        );
+    }
+
+    public function scopeIsAdmin($query)
+    {
+        $query->where('is_admin', true);
+    }
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new ActiveScope);
+    }
 
     public function posts()
     {
