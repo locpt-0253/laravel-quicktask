78a1b0b5bfce2b3358a18e6228e68483d907a1cc
MatsuoRotsuki <loclienhadonganh@gmail.com>
2023-08-31 15:49:40 +0700

    [Chapter 8]: Auth

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'unique:'.User::class, 'max:255', 'regex:/^[a-zA-Z0-9_]+$/'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = new User();
+
+        $user->is_admin = false;
+        $user->is_active = true;
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        
+        $user->save();
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,14 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/users', UserController::class)->middleware('auth.admin');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::name('posts.')->prefix('/posts')->group(function () {
-    Route::get('/', [PostController::class, 'index'])->name('index');
-    Route::get('/create', [PostController::class, 'create'])->name('create');
-    Route::post('/', [PostController::class, 'store'])->name('store');
-    Route::get('/{post}', [PostController::class, 'show'])->name('show');
-    Route::get('/{post}/edit', [PostController::class, 'edit'])->name('edit');
-    Route::put('/{post}', [PostController::class, 'update'])->name('update');
-    Route::delete('/{post}', [PostController::class, 'destroy'])->name('destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
